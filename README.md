e3ea74e4f2f2f8ce75ccf0f44616891bb1a87955
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2024-04-27 17:25:05 -0700

    docs: update README.md and documentation
    - Update README.md for end user instruction, badges
    - Add more comments for clarity in src/index.js

--- README.md
+++ README.md
@@ -1,24 +1,73 @@
-# eslint-config-next-flat [![GitHub Actions badge](https://github.com/jeremy-code/eslint-config-next-flat/actions/workflows/ci.yml/badge.svg)](https://github.com/jeremy-code/eslint-config-next-flat/actions/workflows/ci.yml) [![License](https://img.shields.io/github/license/jeremy-code/eslint-config-next-flat)](LICENSE)
+# eslint-config-next-flat
+
+[![GitHub Actions badge][github-actions-badge]][github-actions]
+[![License][license-badge]](LICENSE)
+[![License][npm-version-badge]][npm-package]
+
+Flat config port of the Next.js ESLint config [`eslint-config-next`](https://www.npmjs.com/package/eslint-config-next).
 
 ## Installation
 
 ```bash
-git clone https://github.com/jeremy-code/eslint-config-next-flat.git
-cd eslint-config-next-flat
-corepack enable
-pnpm install
+npm install -D eslint eslint-config-next-flat   # npm
+yarn add -D eslint eslint-config-next-flat      # yarn
+pnpm install -D eslint eslint-config-next-flat  # pnpm
 ```
 
-## Usage
+Create an `eslint.config.js` file in the root of your Next.js project and add the following:
 
-```bash
-# Set Node.js version
-nvm use
+```js
+const js = require("@eslint/js");
+const next = require("eslint-config-next-flat");
 
-# Build
-pnpm build
+/** @type {import('eslint').Linter.FlatConfig}[] */
+module.exports = [
+  { ignore: [".next"] }, // Change if `distDir` in Next.js config is different
+  js.configs.recommended,
+  next,
+];
+```
+
+If not using TypeScript, add custom Next.js Babel parser from `"next/dist/compiled/babel/eslint-parser"`. Otherwise, for TypeScript, use [`typescript-eslint`](https://typescript-eslint.io/getting-started/).
+
+```js
+const js = require("@eslint/js");
+const next = require("eslint-config-next-flat");
+const parser = require("next/dist/compiled/babel/eslint-parser");
+
+/** @type {import('eslint').Linter.FlatConfig}[] */
+module.exports = [
+  { ignore: [".next"] }, // Change if `distDir` in Next.js config is different
+  js.configs.recommended,
+  next,
+  {
+    files: ["**/*.js", "**/*.mjs"],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        requireConfigFile: false,
+        sourceType: "module",
+        allowImportExportEverywhere: true,
+        babelOptions: {
+          presets: ["next/babel"],
+          caller: {
+            supportsTopLevelAwait: true,
+          },
+        },
+      },
+    },
+  },
+];
 ```
 
 ## License
 
-This project is licensed under the [MIT license](LICENSE).
+`next-config-next-flat` is licensed under the [MIT license](LICENSE).
+
+<!-- Badges -->
+
+[github-actions-badge]: https://github.com/jeremy-code/eslint-config-next-flat/actions/workflows/ci.yml/badge.svg
+[github-actions]: https://github.com/jeremy-code/eslint-config-next-flat/actions/workflows/ci.yml
+[license-badge]: https://img.shields.io/github/license/jeremy-code/eslint-config-next-flat
+[npm-version-badge]: https://img.shields.io/npm/v/eslint-config-next-flat
+[npm-package]: https://npmjs.com/package/eslint-config-next-flat
--- src/index.js
+++ src/index.js
@@ -9,36 +9,47 @@ import globals from "globals";
 export default {
 	plugins: {
 		/**
-		 * Plugins that aren't patched for Next.js (e.g. have metadata property) are
-		 * not usable with --cache or --print-config
-		 *
+		 * Plugins that aren't patched for ESLint flat config (e.g. have metadata
+		 * property) are not usable with --cache or --print-config. All plugins used
+		 * have yet to be patched.
 		 * {@see https://eslint.org/docs/latest/extend/plugin-migration-flat-config#adding-plugin-meta-information}
 		 */
-		import: eslintImport, // not patched
+		import: eslintImport,
 		react,
-		"jsx-a11y": jsxA11y, // not patched
-		"react-hooks": reactHooks, // not patched
-		"@next/next": next, // not patched
+		"jsx-a11y": jsxA11y,
+		"react-hooks": reactHooks,
+		"@next/next": next,
 	},
 	rules: {
 		...react.configs.recommended.rules,
-		...react.configs["jsx-runtime"].rules, // jsx-runtime for react-in-jsx-scope
+		...react.configs["jsx-runtime"].rules, // Allow `react-in-jsx-scope` in React 17
 		...reactHooks.configs.recommended.rules,
 		...next.configs.recommended.rules,
-		// when using @vercel/og (which is bundled into Next.js), tw prop is possible
-		// see https://github.com/vercel/next.js/blob/canary/packages/next/src/compiled/%40vercel/og/types.d.ts#L115
+
+		/**
+		 * `@vercel/og` (which is bundled into Next.js) uses `tw` prop
+		 * {@see https://github.com/vercel/next.js/blob/canary/packages/next/src/compiled/%40vercel/og/types.d.ts#L115}
+		 * {@see https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/no-unknown-property.md}
+		 */
 		"react/no-unknown-property": ["error", { ignore: ["tw"] }],
 	},
 
 	languageOptions: {
-		globals: {
-			...globals.browser,
-			...globals.node,
-		},
-		// No custom parser options or Babel parser, as typescript-esLint & Babel seem to have some conflicts, see https://github.com/typescript-eslint/typescript-eslint/issues/3517
+		/**
+		 * Config originally set browser + node globals. FlatConfig standard is to
+		 * not set globals, and have end users configure them themselves.
+		 * {@see https://eslint.org/blog/2022/08/new-config-system-part-2/#goodbye-environments%2C-hello-globals}
+		 */
+		globals: { ...globals.browser, ...globals.node },
+		/**
+		 * Legacy `eslint-config-next` uses custom Next.js parser + parserOptions
+		 * with Babel by default, and sets parser '@typescript-eslint/parser` for
+		 * .ts, .tsx files. This is not possible in one config with FlatConfig, and
+		 * doing that would result in conflicts between the two parsers.
+		 */
 		parserOptions: {
 			ecmaFeatures: {
-				jsx: true, // set true by eslint-plugin-react default config
+				jsx: true, // set true by eslint-plugin-react's recommended config
 			},
 		},
 	},
@@ -46,6 +57,8 @@ export default {
 		react: {
 			version: "detect",
 		},
+		// Settings for eslint-plugin-import, may be better to use the
+		// React/TypeScript eslint-plugin-import configs instead
 		"import/parsers": {
 			"@typescript-eslint/parser": [".ts", ".mts", ".cts", ".tsx", ".d.ts"],
 		},
